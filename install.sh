1ed76df9dcb57206bc469550a216269ef14c6458
Adam Schodde <aaschodd@asu.edu>
2014-09-25 18:24:06 -0700

    Added shell script for install.

---
+++ install.sh
@@ -0,0 +1,8 @@
+green=$(tput setaf 2)
+reset=$(tput sgr0)
+echo "Copying Development Template..."
+cp server/config/environment/development.js.template server/config/environment/development.js
+echo "Test config will remain the same..."
+echo "${green}Don't forget to update the settings files within lib/config/env.${reset}"
+echo "${green}Make sure to set NODE_ENV environment variable to development.${reset}"
+echo "${green}Make sure to run node install.js after the settings files are updated.${reset}"
