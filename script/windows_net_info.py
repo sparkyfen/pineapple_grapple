87808770cf7705a684560a21cb41c5bfa75d7fb7
Lincoln Turley <lincoln.turley@gmail.com>
2014-10-18 23:36:40 -0700

    Clean up windows_net_info.py script

--- script/windows_net_info.py
+++ script/windows_net_info.py
@@ -3,54 +3,53 @@
 This script is designed for use in Windows systems.
 It will do the following:
 1. Execute the command "netsh wlan show interfaces" in the command prompt (cmd)
-2. Store the ouput into the file netinfo.txt
-3. Parse the gerenated
+2. Retrieve the output
+3. Parse the gerenated output and return an object containing information regarding the network the device is connected to.
 """
 
 import subprocess
 
+def myFunction():
+    # Open a cmd process to execute cmd command.
+    p1 = subprocess.Popen(["cmd", "/C", "netsh wlan show interfaces"], stdout=subprocess.PIPE)
+    raw_network_info = p1.communicate()[0]
 
-def get_net_info(x):
-    return x.split(': ')[1].rstrip()
+    # split ouput from netsh command into array of individual lines.
+    str_array = raw_network_info.splitlines()
 
+    client_mac = ''
+    ssid = ''
+    bssid = ''
+    security_type = ''
 
-f = open("netinfo.txt", 'w')
+    # Loop through each line of output. Extract relevant data
+    for line in str_array:
+        line = line.lower().lstrip()
+        if line.startswith("physical address"):
+            client_mac = line.split(': ')[1].rstrip()
+        if line.startswith("ssid"):
+            ssid = line.split(': ')[1].rstrip()
+        if line.startswith("bssid"):
+            bssid = line.split(': ')[1].rstrip()
+        if line.startswith("authentication"):
+            security_type = line.split(': ')[1].rstrip()
+            #Standardize Security type
+            if security_type == 'wpa2-enterprise':
+                security_type = 'WPA2 Enterprise'
+            elif security_type == 'wpa2-personal':
+                security_type = 'WPA2 Personal'
+            elif security_type == 'wpa-enterprise':
+                security_type = 'WPA Enterprise'
+            elif security_type == 'wpa-personal':
+                security_type = 'WPA Personal'
+            elif security_type == 'wep':
+                security_type = 'WEP'
+            elif security_type == 'open':
+                security_type = 'None'
 
-# subprocess.Popen(['cmd.exe', '/c', 'netsh wlan show interfaces \> text.txt'])
-p = subprocess.Popen(['cmd.exe', '/c', 'netsh wlan show interfaces'], stdout=f)
-p.wait(100)
+    return_value = {'ssid': ssid, 'bssid': bssid, 'security_type': security_type, 'client_mac': client_mac}
+    #if (Verbose_Output):
+        #print(return_value)
+    return return_value
 
-f.close()
-
-network_info = ''
-physical_addr = ''
-ssid = ''
-bssid = ''
-authentication = ''
-
-f2 = open("netinfo.txt", 'r')
-for line in f2:
-    line = line.upper().lstrip()
-    # print(l2)
-    if line.startswith("PHYSICAL ADDRESS"):
-        network_info += line
-        physical_addr = get_net_info(line)
-    if line.startswith("SSID"):
-        network_info += line
-        ssid = get_net_info(line)
-    if line.startswith("BSSID"):
-        network_info += line
-        bssid = get_net_info(line)
-    if line.startswith("AUTHENTICATION"):
-        network_info += line
-        authentication = get_net_info(line)
-
-f2.close()
-
-print(network_info)
-print()
-print()
-print("CLIENT MAC ADDRESS: " + physical_addr)
-print("SSID: " + ssid)
-print("BSSID: " + bssid)
-print("AUTHENTICATION: " + authentication)
+print(myFunction())
