72b44668f0f216beea0a9d7843e36a72d9409920
Adam Schodde <aaschodd@asu.edu>
2014-10-21 00:49:29 -0700

    Updated python script to include logic for comparing db records, calculating traceroute hops, and started on dns lookups.

--- script/main.py
+++ script/main.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
+# Steps the script takes:
+# 1. Get record of current connected AP.
+# 2. Check security against the security on file.
+# 3. Calculate first 3 hops.
+# 4. DNS Lookups on known sites.
+# 5. Validate cetificates for well known sites.
 import netifaces as ni
 import urllib
 import logging
@@ -7,8 +13,11 @@ from requests import get
 from requests import post
 import argparse
 import network_info
+from operator import itemgetter
+import json
 
-URL = 'https://pineapple-grapple.herokuapp.com'
+# URL = 'https://pineapple-grapple.herokuapp.com'
+URL = 'http://localhost:9000'
 ADD_RECORD_ENDPOINT = URL + '/api/ap/addRecord'
 GET_RECORD_ENDPOINT = URL + '/api/ap/getRecord'
 DNS_QUERY_ENDPOINT = URL + '/api/dns/query'
@@ -23,26 +32,10 @@ if args.verbose:
 
 net_info = network_info.NetworkInfo()
 
-for interface in ni.interfaces():
-    if interface == "lo0":
-        continue
-    addrs = ni.ifaddresses(interface)
-    if ni.AF_INET in addrs:
-        mac_address = addrs[ni.AF_LINK][0]["addr"]
-        internal_address = addrs[ni.AF_INET][0]["addr"]
-        broadcast_address = addrs[ni.AF_INET][0]["broadcast"]
-        subnet_mask = addrs[ni.AF_INET][0]["netmask"]
-        print 'mac_address', mac_address
-        print 'internal_address', internal_address
-        print 'broadcast_address', broadcast_address
-        print 'subnet_mask', subnet_mask
-        print 'interface', interface
-        print "\n"
-
 # TODO Switch this to SSL when the new certificate is in place.
 # https://github.com/rdegges/ipify-api/issues/1
 public_ip = get("http://api.ipify.org").text
-print 'public_ip', public_ip
+logging.debug('public_ip: ' + public_ip)
 
 if platform == 'linux' or platform == 'linux2':
     network_info = net_info.get_linux_net_info()
@@ -53,10 +46,55 @@ elif platform == 'win32' or platform == 'cygwin':
 
 if network_info is None:
     sys.exit(1)
-payload = {"ssid": network_info['ssid'], "apMac": network_info['bssid'], "clientMac": network_info['client_mac'], "securityType": network_info['security_type'], "publicIP": public_ip}
+get_record_payload = {"apMac": network_info['bssid']}
 headers = {"content-type": "application/x-www-form-urlencoded"}
-req = post(ADD_RECORD_ENDPOINT, urllib.urlencode(payload), headers=headers)
-if req.status_code is 200:
-    print 'Complete.'
+get_record_req = post(GET_RECORD_ENDPOINT, urllib.urlencode(
+    get_record_payload), headers=headers)
+if get_record_req.status_code is 200:
+    ap_record = get_record_req.json()
+    if ap_record['securityType'] != network_info['security_type']:
+        print 'Security types of known network are different.'
+        print 'Current type is ' + network_info['security_type'] + ' and recorded type is ' + ap_record['securityType']
+    elif ap_record['publicIP'] != public_ip:
+        print 'Public IPs of known network are different.'
+        print 'Current IP address is ' + public_ip + ' and recorded address is ' + ap_record['publicIP']
+    else:
+        hops = net_info.calculate_hops()
+        hop_difference = []
+        for x in hops:
+            if x not in ap_record['hops']:
+                hop_difference.append(x)
+        if len(hop_difference) is not 0:
+            print 'Traceroute hops are different.'
+            print 'Current hops are ' + hops + ' and recorded hops are ' + ap_record['hops']
+        else:
+            dns_check_payload = {"domain": net_info.get_common_domains()}
+            dns_check_req = post(DNS_QUERY_ENDPOINT, urllib.urlencode(
+                dns_check_payload, True), headers=headers)
+            if dns_check_req.status_code is 200:
+                # TODO Compare each domain with their addresses.
+                address_list = dns_check_req.json()
+                print json.dumps(address_list)
+                local_address_list = net_info.lookup_domains()
+                print json.dumps(local_address_list)
+
+            else:
+                print dns_check_req.json()['message']
+                print dns_check_req.raise_for_status()
 else:
-    print req.raise_for_status()
+    hops = net_info.calculate_hops()
+    # TODO No record in DB so we need to do DNS lookups and cert validation.
+    add_record_payload = {
+        "ssid": network_info['ssid'],
+        "apMac": network_info['bssid'],
+        "clientMac": network_info['client_mac'],
+        "securityType": network_info['security_type'],
+        "publicIP": public_ip, "hops": hops
+    }
+    add_record_req = post(ADD_RECORD_ENDPOINT, urllib.urlencode(
+        add_record_payload, True), headers=headers)
+    if add_record_req.status_code is 200:
+        print 'Complete.'
+    else:
+        print add_record_req.json()['message']
+        print add_record_req.raise_for_status()
--- script/network_info.py
+++ script/network_info.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import xmltodict
 import logging
+import socket
 from subprocess import check_output
 
 
@@ -8,6 +9,8 @@ class NetworkInfo(object):
     """Parses network information for a requested OS to retreive the
         SSID, BSSID, Security Type, Client Mac Address"""
     def __init__(self):
+        self.icmp = socket.getprotobyname('icmp')
+        self.udp = socket.getprotobyname('udp')
         pass
 
     def __str__(self):
@@ -26,14 +29,21 @@ class NetworkInfo(object):
 
             ssid = network_ap_values[0]
             bssid = network_ap_values[1]
-            security_type = self._convert_security_type(network_ap_values[5])
-
-            return {
-                'ssid': ssid,
-                'bssid': bssid,
-                'security_type': security_type,
-                'client_mac': wireless_mac_address
-            }
+            security_types = self._get_security_types()
+            security_type = None
+            for index, ap_value in enumerate(network_ap_values):
+                if ap_value in security_types:
+                    security_type = self._convert_security_type(ap_value)
+            if security_type is None:
+                logging.error('Could not find security type in network information.')
+                return None
+            else:
+                return {
+                    'ssid': ssid,
+                    'bssid': bssid,
+                    'security_type': security_type,
+                    'client_mac': wireless_mac_address
+                }
         else:
             logging.error('Network disabled or disconnected.')
             return None
@@ -46,12 +56,61 @@ class NetworkInfo(object):
         logging.error('TODO')
         return None
 
-    def _convert_security_type(self, security_type):
-        security_levels = {
+    def calculate_hops(self):
+        ttl = 1
+        hops = []
+        while ttl < 4:
+            recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, self.icmp)
+            send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, self.udp)
+            send_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+            recv_socket.bind(("", 33434))
+            send_socket.sendto("", ('8.8.8.8', 33434))
+            curr_addr = None
+            curr_name = None
+            try:
+                # socket.recvfrom() gives back (data, address), but we
+                # only care about the latter.
+                _, curr_addr = recv_socket.recvfrom(512)
+                curr_addr = curr_addr[0]  # address is given as tuple
+                try:
+                    curr_name = socket.gethostbyaddr(curr_addr)[0]
+                except socket.error:
+                    curr_name = curr_addr
+            except socket.error:
+                pass
+            finally:
+                send_socket.close()
+                recv_socket.close()
+            if curr_addr is not None:
+                curr_host = curr_addr
+            else:
+                curr_host = None
+            ttl += 1
+            hops.append(curr_host)
+        return hops
+
+    def get_common_domains(self):
+        # TODO Make this list a queryable API endpoint.
+        return ["google.com", "chase.com", "wellsfargo.com", "bankofamerica.com"]
+
+    def lookup_domains(self):
+        domains = self.get_common_domains()
+        address_list = []
+        for domain in domains:
+            addr_info = socket.gethostbyname_ex(domain)
+            address_info_list = addr_info[2]
+            address_list.append({"domain": domain, "addresses": address_info_list})
+        return address_list
+
+    def _get_security_types(self):
+        return {
             "spairport_security_mode_none": "none",
             "spairport_security_mode_wep": "WEP",
             "spairport_security_mode_wpa_personal": "WPA Personal",
             "spairport_security_mode_wpa2_personal": "WPA2 Personal",
             "spairport_security_mode_wpa2_enterprise": "WPA2 Enterprise"
         }
+
+    def _convert_security_type(self, security_type):
+        security_levels = self._get_security_types()
         return security_levels[security_type]
