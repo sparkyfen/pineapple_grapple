6ca411573c777c1873d04feebcbecb27ccf064a0
Lincoln Turley <lincoln.turley@gmail.com>
2014-11-15 14:23:12 -0700

    Added setup.py

---
+++ script/setup.py
@@ -0,0 +1,12 @@
+__author__ = 'Lincoln Turley'
+from setuptools import setup
+setup(name='pagrapple',
+      description='A project for CSE 465 (Information Assurance) to detect MITM attacks.',
+      author='Lester Penning, Adam Schodde, Lincoln Turley',
+      author_email='aaschodd@asu.edu',
+      url='http://www.pagrapple.com/',
+      version='1.0',
+      py_modules=['main', 'network_info','api'],
+      platforms=['Windows', 'iOS', 'Linux'],
+      install_requires=['netifaces', 'requests', 'netaddr', 'xmltodict'],
+      )
